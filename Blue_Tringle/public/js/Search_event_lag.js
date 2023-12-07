a3f5d9e06582287dd14b63543490441bb93cbc80
Zainab Nadeem Fazaa Alshubati <zainab.nadeem2015@gmail.com>
2023-12-07 15:24:32 +0300

    Initial commit

---
+++ Blue_Tringle/public/js/Search_event_lag.js
@@ -0,0 +1,14 @@
+function search(event) {
+    event.preventDefault();
+    var searchInput = document.getElementById('search-input');
+    var searchTerm = searchInput.value;
+    // Perform the search operation based on the search term
+    // ...
+  }
+  
+  function changeLanguage(event) {
+    var languageSelect = document.getElementById('language-select');
+    var selectedLanguage = languageSelect.value;
+    // Perform any language-specific operations or translations based on the selected language
+    // ...
+  }
---
+++ Blue_Tringle/resources/views/welcome.blade.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="content-language" content="en,de,fr,Arb" />
+
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Blue Tringle</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://fonts.googleapis.com/css?family=Cairo' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="javescrupt" href="{{ asset('js/Search_event_lag.js') }}">
+   
+  </head>
+  <body>
+<div class="container">
+       
+        <!-- logo begin -->
+           <div id="logo" >
+            <a href="#">
+                <img class="right" src="{{ asset('images/LOGO.svg') }}" alt="yy">
+            </a>
+          </div>
+        <!-- logo close -->
+      <div class="Zano">  
+        <section class="navbar-Right">
+          <span class="overlay"></span>
+          <div class="menu" id="menu">
+             <div class="menu-header">
+                <span class="menu-arrow"><i class="bx bx-chevron-left"></i></span>
+                <span class="menu-title"></span>
+             </div>
+             <ul class="menu-inner">
+                <li class="menu-item"><a href="#" class="menu-link">الرئسية</a></li>
+                <li class="menu-item"><a href="#" class="menu-link">من نحن</a></li>
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link">الوكالات<i class="bx bx-chevron-right"></i></span>
+                   <div class="submenu megamenu megamenu-column-4">
+                      <div class="submenu-inner">
+                         <h4 class="submenu-title">بوب كات</h4>
+                         <ul class="submenu-list">
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 1 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 2 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 3 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 4 </a></li>
+                         </ul>
+                         <h4 class="submenu-title">ديسن</h4>
+                         <ul class="submenu-list">
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 1 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 2 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 3 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 4 </a></li>
+                         </ul>
+                      </div>
+                      <div class="submenu-inner">
+                         <h4 class="submenu-title">بوب كات</h4>
+                         <ul class="submenu-list">
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 1 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 2 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 3 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 4 </a></li>
+                         </ul>
+                         <h4 class="submenu-title">بوب كات</h4>
+                         <ul class="submenu-list">
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتح 1 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 2 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 3 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 4 </a></li>
+                         </ul>
+                      </div>
+                      <div class="submenu-inner">
+                         <h4 class="submenu-title">ديسن</h4>
+                         <ul class="submenu-list">
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 1 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 2 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 3 </a></li>
+                            <li class="submenu-item"><a href="#" class="submenu-link">منتج 4 </a></li>
+                         </ul>
+                      </div>
+                      <div class="submenu-inner">
+                         <img src="https://i.ibb.co/3Bj3gbS/Product-1.jpg" class="submenu-image" alt="Product">
+                      </div>
+                   </div>
+                </li>
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link">المعرض<i class="bx bx-chevron-right"></i></span>
+                   <div class="submenu megamenu megamenu-column-4">
+                      <div class="submenu-inner">
+                         <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/kgNX8ks/Product-2.jpg" class="submenu-image" alt="Product">
+                            <span class="submenu-title">Product Name</span>
+                         </a>
+                      </div>
+                      <div class="submenu-inner">
+                         <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/ZTD2wF6/Product-3.jpg" class="submenu-image" alt="Product">
+                            <span class="submenu-title">Product Name</span>
+                         </a>
+                      </div>
+                      <div class="submenu-inner">
+                         <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/prb0Vz9/Product-4.jpg" class="submenu-image" alt="Product">
+                            <span class="submenu-title">Product Name</span>
+                         </a>
+                      </div>
+                      <div class="submenu-inner">
+                         <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/zPJm9jy/Product-5.jpg" class="submenu-image" alt="Product">
+                            <span class="submenu-title">Product Name</span>
+                         </a>
+                      </div>
+                   </div>
+                </li>
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link">الوظائف <i class="bx bx-chevron-right"></i></span>
+                   <div class="submenu megamenu megamenu-column-1">
+                      <ul class="submenu-list">
+                         <li class="submenu-item"><a href="#" class="submenu-link">1
+             <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/zPJm9jy/Product-5.jpg" class="submenu-image_bronds" alt="Product">
+                            <span class="submenu-title">2</span>
+                         </a>
+             </a></li>
+                         <li class="submenu-item"><a href="#" class="submenu-link">3
+             <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/zPJm9jy/Product-5.jpg" class="submenu-image_bronds" alt="Product">
+                            <span class="submenu-title">3 </span>
+                         </a></a></li>
+                         <li class="submenu-item"><a href="#" class="submenu-link">4
+             <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/zPJm9jy/Product-5.jpg" class="submenu-image_bronds" alt="Product">
+                            <span class="submenu-title">5</span>
+                         </a></a></li>
+                         <li class="submenu-item"><a href="#" class="submenu-link">6
+             <a href="#" class="submenu-link">
+                            <img src="https://i.ibb.co/zPJm9jy/Product-5.jpg" class="submenu-image_bronds" alt="Product">
+                            <span class="submenu-title">7</span>
+                         </a></a></li>
+                      </ul>
+                   </div>
+                </li>
+                <li class="menu-item"><a href="#" class="menu-link">تواصل معنا </a></li>
+             </ul>
+          </div>
+          <div class="search-container submenu-inner">
+            <form action="/action_page.php">
+              <input type="text" placeholder="البحث عن" name="search">
+              <i class="fa fa-search"></i>
+            </form>
+          </div>
+          <div class="boxdiv">
+            <input type="text" placeholder= "العربية " name="search">
+            <i class='fa fa-globe'></i>
+          </div>
+       </section>
+  </div>
+  <div class="text-container">
+    <div class="card-image"> <img class="left  direction: ltr" src="{{ asset('images/Layer_1.png') }}" alt="yy"></div>
+  </div>
+  <div class="text-container">
+ 
+   <div > 
+     
+      <div style="display: flex; justify-content: space-between; font-family: 'Cairo';">
+         <div  class="left ;size:20%;"style="display: flex; justify-content: center; align-items: center;">
+           <!-- Content of the left div -->
+           <h1>           &nbsp;</h1>
+           <img src="{{ asset('images/BLUE_T.svg') }}" alt="yy">
+         </div>
+         <div style="text-align: justify; " class=" right ; size:20%; direction: rtl;">
+           <!-- Content of the right div -->
+        <h1  style="direction: rtl;"> <span style='font-size:20 px;direction: rtl;'> &#9664; من نحن ـــ؟ </span></h1>  
+           <p style="direction: rtl;">
+             شركة بلو ترانجل للتجارة المحدودة عضو في مجموعة شركات عبد الله الرباعي التي تتخصص في عدة مجالات مختلفة
+             مثل المقاولات والزراعة وأيضًا التجارة العامة تأسست الشركة في عام 2015 بشكل رسمي في الجمهورية اليمنية
+             وتمكنت من بناء خبرة قوية وجيدة خلال السنوات منذ تأسيس شركة بلوتراتجل للتجارة المحدودة تخصصت الشركة في
+             معدات البناء وآلات الحفر وأيضًا في مجال الطاقة، المعدات الزراعية معدات الرفع وأيضًا معدات واللات خاصة
+             وقد أنشأت الشركة شبكة قوية جدًا وموثوق بها داخل وخارج اليمن مع العملاء والموردين قامت الشركة بتوسيع محفظة
+             منتجاتها لتصبح موزعًا حصريًا لأكبر العلامات التجارية مثل شركة ديفلون الكورية (معدات دوسان الثقيلة سابقًا)
+             ، شركة بوبكات التشيكية معدات البناء والحفر الخفيفة والمتوسطة) شركة بوماج الألمانية (معدات الأسفلت الثقيلة
+             والخفيفة ، شركة فارم ترك من اسكرت كابوتا الهندية (حراثات زراعية خفيفة ومتوسطة شركة كوباز الكورية (معدات
+             صيانة الطرقات وغيرها من العلامات التجارية الرائدة في المجال.
+           </p>
+         </div>
+       </div>
+      </span>
+   </div>
+ 
+ </div>
+ <br />
+ <div class="text-container">
+   <div class="row">
+      
+</div>
+
+</div>
+<br />
+<section style="  display: flex;justify-content: center; flex-wrap: wrap; max-width: 75em; margin-inline: auto;transform: translatey(-3em);position: relative; z-index: 10;">
+   <div class="card-top">
+     <div class="card" >
+       <img src="" alt="Alexandra Smith">
+       <h2>Alexandra Smith</h2>
+       <p>Founder and Chief Operations Officer</p>
+       <button>More Info</button>
+     </div>
+     <div class="card" style="border-left: 1px solid white;">
+       <img src="" alt="John Smith">
+       <h2>John Smith</h2>
+       <p>Founder and Chief Executive Officer</p>
+       <button>More Info</button>
+     </div>
+     <div class="card" style="border-left: 1px solid white;">
+      <img src="" alt="John Smith">
+      <h2>John Smith</h2>
+      <p>Founder and Chief Executive Officer</p>
+      <button>More Info</button>
+    </div>
+   </div>
+
+   <div class="card-top">
+      <div class="card" >
+        <img src="" alt="Alexandra Smith">
+        <h2>Alexandra Smith</h2>
+        <p>Founder and Chief Operations Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+        <img src="" alt="John Smith">
+        <h2>John Smith</h2>
+        <p>Founder and Chief Executive Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+       <img src="" alt="John Smith">
+       <h2>John Smith</h2>
+       <p>Founder and Chief Executive Officer</p>
+       <button>More Info</button>
+     </div>
+    </div>
+
+    <div class="card-top">
+      <div class="card" >
+        <img src="" alt="Alexandra Smith">
+        <h2>Alexandra Smith</h2>
+        <p>Founder and Chief Operations Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+        <img src="" alt="John Smith">
+        <h2>John Smith</h2>
+        <p>Founder and Chief Executive Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+       <img src="" alt="John Smith">
+       <h2>John Smith</h2>
+       <p>Founder and Chief Executive Officer</p>
+       <button>More Info</button>
+     </div>
+    </div>
+
+    <div class="card-top">
+      <div class="card" >
+        <img src="" alt="Alexandra Smith">
+        <h2>Alexandra Smith</h2>
+        <p>Founder and Chief Operations Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+        <img src="" alt="John Smith">
+        <h2>John Smith</h2>
+        <p>Founder and Chief Executive Officer</p>
+        <button>More Info</button>
+      </div>
+      <div class="card" style="border-left: 1px solid white;">
+       <img src="" alt="John Smith">
+       <h2>John Smith</h2>
+       <p>Founder and Chief Executive Officer</p>
+       <button>More Info</button>
+     </div>
+    </div>
+
+ </section>
+</div>
+  <div class="gradient-bg">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+          <feBlend in="SourceGraphic" in2="goo" />
+        </filter>
+      </defs>
+    </svg>
+    <div class="gradients-container">
+     <div class="g1"></div>
+      <div class="g2"></div>
+      <div class="g3"></div>
+      <div class="g4"></div> 
+      <div class="g3"></div>
+     
+    </div>
+  </div>
+ 
+
+
+     <link rel="javescrupt" href="{{ asset('js/wow.min.js') }}">
+  </body>
+</html>
